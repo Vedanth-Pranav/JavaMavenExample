7ea66b7c1d4cff862c98bd808c5ec12fe5918b14
Pranav <narayanapranav007@gmail.com>
2024-11-14 12:19:57 +0530

    Sample Maven Java

---
+++ src/main/java/com/app/demo/javamavenexample/App.java
@@ -0,0 +1,15 @@
+package com.app.demo.javamavenexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Vedanth Pranav Reddy");
+        System.out.println("22BD1A0557");
+    }
+}
